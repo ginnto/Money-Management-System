78e849dc0be15984bb84dd95e3424fff79659a9b
ginnto <ginto252@gmail.com>
2024-12-11 17:27:45 +0530

    last update - static,temp

--- MoneyManagementSystem/home/views.py
+++ MoneyManagementSystem/home/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import IncomeSource
+from .forms import IncomeSourceForm
 
-# Create your views here.
+def income_list(request):
+    incomes = IncomeSource.objects.filter(user=request.user)
+    return render(request, 'income/income_list.html', {'incomes': incomes})
+
+def add_income(request):
+    if request.method == 'POST':
+        form = IncomeSourceForm(request.POST)
+        if form.is_valid():
+            income = form.save(commit=False)
+            income.user = request.user
+            income.save()
+            return redirect('income_list')
+    else:
+        form = IncomeSourceForm()
+    return render(request, 'income/add_income.html', {'form': form})
+
+def delete_income(request, pk):
+    income = get_object_or_404(IncomeSource, pk=pk, user=request.user)
+    if request.method == 'POST':
+        income.delete()
+        return redirect('income_list')
+    return render(request, 'income/delete_income.html', {'income': income})
